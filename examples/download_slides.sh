fbb2c308bf3db1c6921e7773061a0dc4d2a154b2
r <r>
2023-08-01 19:26:33 +0200

    selected slides

---
+++ examples/download_slides.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./gdc_client download -m bladder.tsv -d bladder
+./gdc_client download -m brain.tsv -d brain
+./gdc_client download -m esophagus.tsv -d esophagus
+./gdc_client download -m gallblader.tsv -d gallblader
+./gdc_client download -m kidney.tsv -d kidney
+./gdc_client download -m lymph_nodes.tsv -d lymph_nodes
+./gdc_client download -m ovarian.tsv -d ovarian
+./gdc_client download -m pancreas.tsv -d pancreas
+./gdc_client download -m skin.tsv -d skin
+./gdc_client download -m testis.tsv -d testis
+./gdc_client download -m thyroid.tsv -d thyroid
+./gdc_client download -m uterus.tsv -d uterus
+```
